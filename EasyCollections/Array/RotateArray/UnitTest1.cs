e0dd2743394e8d4f60856915de36971b79a1cc2d
Raymond <raymond@frogparking.com>
2022-11-18 16:55:18 +1300

    rotate array

---
+++ EasyCollections/Array/RotateArray/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace RotateArray;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Worker.Test();
+        Assert.Pass();
+    }
+}
